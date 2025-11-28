8530cb8266b4f37dc66bda7e8415f330fbadad5a
fedecorra11 <fede.corradi2011@gmail.com>
2025-11-28 10:54:29 +0000

    28/11/25

--- es1.c
+++ es1.c
@@ -5,4 +5,17 @@
 int main(){
 
     return 0;
+     float num1, num2; 
+    printf("scegli due numeri");
+    scanf("%e", &num1); 
+    scanf("%e", &num2); 
+
+    if (num1 > num2 )
+    printf(" il primo numero è maggiore del secondo"); 
+    
+    else {
+        (num2 > num1);
+        printf("il secondo numero è maggiore del primo");
+    }
+
 }
